ab183ff3cb7e0fe84b12a69ab71effe73d610f39
Aniruddha Bagal <aniruddhabagal09@gmail.com>
2022-04-12 20:46:27 +0530

    Create HashSet(Q1 4b).java

---
+++ HashSet(Q1 4b).java
@@ -0,0 +1,21 @@
+/*
+QPaper-1 4b: Analyze the below code and complete the program to add 5 elements and print all the elements of the collection.
+*/
+
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args)
+    {
+        HashSet<String> hs = new HashSet<String>();
+        
+        hs.add("A");
+        hs.add("B");
+        hs.add("C");
+        hs.add("D");
+        hs.add("E");
+        
+        System.out.println(hs);
+    }
+}
